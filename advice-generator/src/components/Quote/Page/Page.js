350fa331d8e11340523bd3373a75c20620e57394
Ankit Prakash <prakashankit526@gmail.com>
2023-01-08 18:52:30 +0530

    final commit

--- advice-generator/src/components/Quote/Page/Page.js
+++ advice-generator/src/components/Quote/Page/Page.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './Page.module.css';
 import Quote from '../Quote';
+import quotes from '../../Data';
 
 const Page = () => {
+    const [pos,setPos] = useState(0);
+    const [bgColor, setBgColor] = useState(0);
+    function handleClick(){
+        setPos((pos+1)%quotes.length);
+        setBgColor((bgColor+1)%3);
+    }
+
+    const bgcolors = ["#001a33","#ff9900","#330000"];
+
     return (
-        <div className={styles.body}>
-            <Quote />
+        <div className={styles.body} style={{backgroundColor:bgcolors[bgColor]}}>
+            <Quote bg={bgColor} pos={pos}/>
+            <div className={styles.dice} onClick={handleClick}>
+                <i className="fa fa-arrows-alt" style={{fontSize:26, color:"red"}}></i>
+            </div>
         </div>
     );
 };
--- advice-generator/src/components/Quote/Quote.js
+++ advice-generator/src/components/Quote/Quote.js
@@ -1,30 +1,19 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styles from './Quote.module.css';
 import quotes from '../Data';
 
-const Quote = () => {
-    const [pos,setPos] = useState(0);
-    const [bgColor, setBgColor] = useState(0);
-    function handleClick(){
-        let newpos = Math.floor((Math.random()* quotes.length));
-        setPos(newpos);
-        setBgColor((bgColor+1)%3);
-    }
-
+const Quote = (props) => {
     const bgcolors = ["#313A49","#B46823","#572525"];
 
     return (
-        <div className={styles.container} style={{backgroundColor: bgcolors[bgColor]}}>
+        <div className={styles.container} style={{backgroundColor: bgcolors[props.bg]}}>
             <div className={styles.title}>
-                ADVICE #117
+                ADVICE #{Math.floor(Math.random()*1000)}
             </div>
             <div className={styles.quote}>
-                "{quotes[pos]}"
+                "{quotes[props.pos]}"
             </div>
             <hr className={styles.hr}></hr>
-            <div className={styles.dice} onClick={handleClick}>
-            <i className="fa fa-arrows-alt" style={{fontSize:26, color:"red"}}></i>
-            </div>
         </div>
     );
 };
