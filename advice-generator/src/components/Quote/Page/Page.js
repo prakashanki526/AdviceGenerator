1964d894d1b745365c05099280d1141070ce2fa4
Ankit Prakash <prakashankit526@gmail.com>
2023-01-09 21:44:37 +0530

    added dice icon

--- advice-generator/src/components/Quote/Page/Page.js
+++ advice-generator/src/components/Quote/Page/Page.js
@@ -16,8 +16,10 @@ const Page = () => {
     return (
         <div className={styles.body} style={{backgroundColor:bgcolors[bgColor]}}>
             <Quote bg={bgColor} pos={pos}/>
-            <div className={styles.dice} onClick={handleClick}>
-                <i className="fa fa-arrows-alt" style={{fontSize:26, color:"red"}}></i>
+            <div className={styles.diceContainer} onClick={handleClick}>
+                {/* <i className="fa fa-arrows-alt" style={{fontSize:26, color:"red"}}></i> */}
+
+                <svg className={styles.dice} width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z" fill="#202733"/></svg>
             </div>
         </div>
     );
